1b9db7b41ed189c9ff0b7bb4c8d3721bade86c63
Nicolas Maloney <nicolasmaloneyf@gmail.com>
2025-01-23 23:48:22 -0300

    "Created variable to get weight and height in MainActivity.kt"

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -2,10 +2,31 @@ package com.comunidadedevspace.imc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Get text input for Height and associate with a variable for UI component<EditText>
+
+        val edtHeight = findViewById<TextInputEditText>(R.id.edtInputHeight)
+
+        // Get text input for Weight and associate with a variable for UI component<EditText>
+
+        val edtWeight = findViewById<TextInputEditText>(R.id.edtWeight)
+
+        // Get caculate button
+
+        val btnCalculate = findViewById<Button>(R.id.calculate_button)
+
+        btnCalculate.setOnClickListener {
+
+            val weight = edtWeight.text
+            val height = edtHeight.text
+        }
     }
 }
